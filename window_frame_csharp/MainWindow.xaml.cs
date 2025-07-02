db98457e60bb0c9eac164431383b5bfba7ab436b
jeffcall-ch <65159016+jeffcall-ch@users.noreply.github.com>
2025-07-02 08:47:38 +0000

    Fix(csharp): Handle maximized windows on multi-monitor setups

--- window_frame_csharp/MainWindow.xaml.cs
+++ window_frame_csharp/MainWindow.xaml.cs
@@ -40,7 +40,6 @@ namespace WindowFramer
             this.Visibility = Visibility.Collapsed;
             await Task.Delay(500); // Give time for window to hide
 
-            // Wait for user to click on the target window
             while (true)
             {
                 if ((NativeMethods.GetAsyncKeyState(0x01) & 0x8000) != 0) // VK_LBUTTON
@@ -75,7 +74,6 @@ namespace WindowFramer
                 _selectedBrush = new SolidColorBrush(color);
                 if (_cts != null && !_cts.IsCancellationRequested)
                 {
-                    // Re-create frames with the new color
                     Dispatcher.Invoke(() =>
                     {
                         CloseFrameWindows();
@@ -144,49 +142,68 @@ namespace WindowFramer
                     break;
                 }
 
-                var result = NativeMethods.DwmGetWindowAttribute(_targetHwnd, NativeMethods.DWMWA_EXTENDED_FRAME_BOUNDS, out var rect, Marshal.SizeOf(typeof(NativeMethods.RECT)));
-                if (result != 0)
+                var placement = new NativeMethods.WINDOWPLACEMENT();
+                placement.length = Marshal.SizeOf(placement);
+                NativeMethods.GetWindowPlacement(_targetHwnd, ref placement);
+
+                NativeMethods.RECT rect;
+                if (placement.showCmd == NativeMethods.SW_SHOWMAXIMIZED)
                 {
-                    NativeMethods.GetWindowRect(_targetHwnd, out rect);
+                    IntPtr monitor = NativeMethods.MonitorFromWindow(_targetHwnd, NativeMethods.MONITOR_DEFAULTTONEAREST);
+                    var monitorInfo = new NativeMethods.MONITORINFO();
+                    monitorInfo.cbSize = Marshal.SizeOf(monitorInfo);
+                    NativeMethods.GetMonitorInfo(monitor, ref monitorInfo);
+                    rect = monitorInfo.rcMonitor;
+                }
+                else
+                {
+                    var result = NativeMethods.DwmGetWindowAttribute(_targetHwnd, NativeMethods.DWMWA_EXTENDED_FRAME_BOUNDS, out rect, Marshal.SizeOf(typeof(NativeMethods.RECT)));
+                    if (result != 0)
+                    {
+                        NativeMethods.GetWindowRect(_targetHwnd, out rect);
+                    }
                 }
 
                 Dispatcher.Invoke(() =>
                 {
-                    // Get the window that is immediately above the target window in the Z-order.
                     IntPtr windowAbove = NativeMethods.GetWindow(_targetHwnd, NativeMethods.GW_HWNDPREV);
                     if (windowAbove == IntPtr.Zero)
                     {
-                        // If there is no window above, it's the topmost, so our frame can be too.
                         windowAbove = NativeMethods.HWND_TOPMOST;
                     }
+
+                    var helper0 = new WindowInteropHelper(_frameWindows[0]);
+                    var helper1 = new WindowInteropHelper(_frameWindows[1]);
+                    var helper2 = new WindowInteropHelper(_frameWindows[2]);
+                    var helper3 = new WindowInteropHelper(_frameWindows[3]);
 
                     // Top
                     _frameWindows[0].Left = rect.Left;
                     _frameWindows[0].Top = rect.Top;
                     _frameWindows[0].Width = rect.Right - rect.Left;
                     _frameWindows[0].Height = FRAME_THICKNESS;
-                    NativeMethods.SetWindowPos(new WindowInteropHelper(_frameWindows[0]).Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
+                    NativeMethods.SetWindowPos(helper0.Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
 
                     // Bottom
                     _frameWindows[1].Left = rect.Left;
                     _frameWindows[1].Top = rect.Bottom - FRAME_THICKNESS;
                     _frameWindows[1].Width = rect.Right - rect.Left;
                     _frameWindows[1].Height = FRAME_THICKNESS;
-                    NativeMethods.SetWindowPos(new WindowInteropHelper(_frameWindows[1]).Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
+                    NativeMethods.SetWindowPos(helper1.Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
 
                     // Left
                     _frameWindows[2].Left = rect.Left;
                     _frameWindows[2].Top = rect.Top;
                     _frameWindows[2].Width = FRAME_THICKNESS;
                     _frameWindows[2].Height = rect.Bottom - rect.Top;
-                    NativeMethods.SetWindowPos(new WindowInteropHelper(_frameWindows[2]).Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
+                    NativeMethods.SetWindowPos(helper2.Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
 
                     // Right
                     _frameWindows[3].Left = rect.Right - FRAME_THICKNESS;
                     _frameWindows[3].Top = rect.Top;
                     _frameWindows[3].Width = FRAME_THICKNESS;
                     _frameWindows[3].Height = rect.Bottom - rect.Top;
-                    NativeMethods.SetWindowPos(new WindowInteropHelper(_frameWindows[3]).Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
+                    NativeMethods.SetWindowPos(helper3.Handle, windowAbove, 0, 0, 0, 0, NativeMethods.SWP_NOMOVE | NativeMethods.SWP_NOSIZE | NativeMethods.SWP_NOACTIVATE);
                 });
 
                 Thread.Sleep(16);
--- window_frame_csharp/NativeMethods.cs
+++ window_frame_csharp/NativeMethods.cs
@@ -29,6 +29,10 @@ public static class NativeMethods
     [DllImport("user32.dll", SetLastError = true)]
     public static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
 
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
     // For setting window position
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
@@ -41,6 +45,14 @@ public static class NativeMethods
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool GetCursorPos(out POINT lpPoint);
 
+    // For Monitor Info
+    [DllImport("user32.dll")]
+    public static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+
     // --- Constants ---
     public static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
     public const uint GA_ROOT = 2;
@@ -49,7 +61,8 @@ public static class NativeMethods
     public const uint SWP_NOSIZE = 0x0001;
     public const uint SWP_NOACTIVATE = 0x0010;
     public const uint GW_HWNDPREV = 3;
-
+    public const uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+    public const int SW_SHOWMAXIMIZED = 3;
 
     // --- Structs ---
     [StructLayout(LayoutKind.Sequential)]
@@ -66,5 +79,26 @@ public static class NativeMethods
         public int Top;
         public int Right;
         public int Bottom;
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT
+    {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public POINT ptMinPosition;
+        public POINT ptMaxPosition;
+        public RECT rcNormalPosition;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct MONITORINFO
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public uint dwFlags;
     }
 }
